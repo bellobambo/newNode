f94bc44c63fdf8c19d70f223c6d892d5e22f4c70
bellobambo <bellobambo21@gmail.com>
2024-05-01 11:06:57 +0100

    server2

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url'
+import path from 'path'
+
+const PORT = process.env.PORT
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer( async (req, res) => {
+
+    try {
+        if (req.method === 'GET') {
+            let filePath
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found')
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html')
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('Method Not Allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('<h1>Server Error</h1>')
+    }
 
 
-const server = http.createServer((req, res)=>{
-res.write('hello world');
-res.end();
-})
 
-server.listen(8000 , () =>{
+
+    console.log(req.url);
+    console.log(req.method);
+});
+
+
+
+server.listen(PORT, () => {
     console.log('server listening on port  8000')
 });
+
